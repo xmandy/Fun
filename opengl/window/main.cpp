1aeaba6a845e30889e8b75ef0a7e9e7e7e995a85
xmandy <shenxm06@gmail.com>
2017-03-08 00:03:45 +0800

    xcode can run now.

--- opengl/window/main.cpp
+++ opengl/window/main.cpp
@@ -1,7 +1,40 @@
-#include <stdio.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-	printf("this is my first cmake xcode project!\n");
+    if(!glfwInit())
+    {
+        std::cout << "glfw init failed!" << std::endl;
+        return 0;
+    }
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    std::cout << "OpenGL Vendor:" << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL Version:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
 	return 0;
 }
