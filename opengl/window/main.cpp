3f0209a2e9e933c4e87d9cdc77d351551f9e7d11
xmandy <shenxm06@gmail.com>
2017-03-19 00:01:47 +0800

    add os specified code

--- opengl/window/main.cpp
+++ opengl/window/main.cpp
@@ -5,12 +5,15 @@
 
 int main(int argc, char **argv)
 {
+    
     if(!glfwInit())
     {
         std::cout << "glfw init failed!" << std::endl;
         return 0;
     }
+#ifdef MAC_OS_X
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
