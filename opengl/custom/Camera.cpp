ddfda13830dd0c3bb8b4099992456bca0ed26d1a
xmandy <shenxm06@gmail.com>
2017-04-30 14:36:48 +0800

    temp camera cls.

--- opengl/custom/Camera.cpp
+++ opengl/custom/Camera.cpp
@@ -1,3 +1,93 @@
 #include "Camera.h"
 
 
+Camera::Camera(glm::vec3 position, glm::vec3 up, GLfloat yaw, GLfloat pitch):
+Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), FieldOfView(FOV)
+{
+    Position = position;
+    WorldUp = up;
+    Yaw = yaw;
+    Pitch = pitch;
+    updateCameraVectors();
+}
+
+Camera::Camera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat upX, GLfloat upY, GLfloat upZ, GLfloat yaw, GLfloat pitch):
+Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), FieldOfView(FOV)
+{
+    Position = glm::vec3(posX, posY, posZ);
+    WorldUp = glm::vec3(upX, upY, upZ);
+    Yaw = yaw;
+    Pitch = pitch;
+    updateCameraVectors();
+}
+
+glm::mat4 Camera::GetViewMatrix()
+{
+    return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
+}
+
+glm::mat4 Camera::GetProjectionMatrix()
+{
+    return glm::perspective(glm::radians(FieldOfView), (GLfloat)ScreenWidth / (GLfloat)ScreenHeight, 0.1f, 100.0f);
+}
+
+// Calculates the front vector from the Camera's (updated) Eular Angles
+void Camera::updateCameraVectors()
+{
+    // Calculate the new Front vector
+    glm::vec3 front;
+    front.x = sin(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
+    front.y = sin(glm::radians(this->Pitch));
+    front.z = cos(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
+    this->Front = glm::normalize(front);
+    // Also re-calculate the Right and Up vector
+    this->Right = glm::normalize(glm::cross(this->Front, this->WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+    this->Up    = glm::normalize(glm::cross(this->Right, this->Front));
+}
+
+    // Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+void Camera::ProcessMouseScroll(GLfloat yoffset)
+{
+    if (this->FieldOfView >= 1.0f && this->FieldOfView <= 45.0f)
+        this->FieldOfView -= yoffset;
+    if (this->FieldOfView <= 1.0f)
+        this->FieldOfView = 1.0f;
+    if (this->FieldOfView >= 45.0f)
+        this->FieldOfView = 45.0f;
+}
+
+// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+void Camera::ProcessMouseMovement(GLfloat xoffset, GLfloat yoffset, GLboolean constrainPitch)
+{
+    xoffset *= this->MouseSensitivity;
+    yoffset *= this->MouseSensitivity;
+    
+    this->Yaw   += xoffset;
+    this->Pitch += yoffset;
+    
+    // Make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (constrainPitch)
+    {
+        if (this->Pitch > 89.0f)
+            this->Pitch = 89.0f;
+        if (this->Pitch < -89.0f)
+            this->Pitch = -89.0f;
+    }
+    
+    // Update Front, Right and Up Vectors using the updated Eular angles
+    this->updateCameraVectors();
+}
+
+// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+void Camera::ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime)
+{
+    GLfloat velocity = this->MovementSpeed * deltaTime;
+    if (direction == FORWARD)
+        this->Position += this->Front * velocity;
+    if (direction == BACKWARD)
+        this->Position -= this->Front * velocity;
+    if (direction == LEFT)
+        this->Position -= this->Right * velocity;
+    if (direction == RIGHT)
+        this->Position += this->Right * velocity;
+}
