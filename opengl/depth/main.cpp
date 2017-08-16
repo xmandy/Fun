17d2a08caffa4209c30ce4fb1c41ad6060dec07a
xmandy <shenxm06@gmail.com>
2017-08-17 00:33:40 +0800

    framebuffer tmp1

--- opengl/depth/main.cpp
+++ opengl/depth/main.cpp
@@ -267,8 +267,60 @@ int main(int argc, char **argv)
 
 
     glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_ALWAYS);
-   
+    //glDepthFunc(GL_ALWAYS);
+    unsigned int renderTexture;
+    glGenTextures(1, &renderTexture);
+    glBindTexture(GL_TEXTURE_2D, renderTexture);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ScreenWidth, ScreenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, ScreenWidth, ScreenHeight);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderTexture, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout<< "frame buffer incomplete!" << std::endl;
+        return 0;
+    }
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    
+    float quadVertices[] = {
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 1.0f, 1.0f,
+    };
+    
+    unsigned int quardVAO, quardVBO;
+    glGenVertexArrays(1, &quardVAO);
+    glGenBuffers(1, &quardVBO);
+    glBindVertexArray(quardVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quardVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2*sizeof(float)));
+    glBindVertexArray(0);
+    
+    
+    
+
     while (!glfwWindowShouldClose(window)) {
 		GLfloat current = glfwGetTime();
 		deltaTime = current - lastFrame;
