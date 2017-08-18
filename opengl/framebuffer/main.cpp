c272618085d11f734e150bcde881298a544aa80c
xmandy <shenxm06@gmail.com>
2017-08-19 01:50:33 +0800

    framebuffer complete.

--- opengl/framebuffer/main.cpp
+++ opengl/framebuffer/main.cpp
@@ -11,8 +11,8 @@
 #include "PlatformUtils.h"
 #include "Camera.h"
 
-int ScreenWidth = 640;
-int ScreenHeight = 480;
+int ScreenWidth = 800;
+int ScreenHeight = 600;
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
@@ -25,14 +25,14 @@ bool keys[1024];
 
 void DoCameraMovements()
 {
-	if (keys[GLFW_KEY_W])
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (keys[GLFW_KEY_S])
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (keys[GLFW_KEY_A])
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (keys[GLFW_KEY_D])
-		camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (keys[GLFW_KEY_W])
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (keys[GLFW_KEY_S])
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (keys[GLFW_KEY_A])
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (keys[GLFW_KEY_D])
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
@@ -41,20 +41,20 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
     {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
-	if (key >= 0 && key < 1024)
-	{
-		if (action == GLFW_PRESS)
-			keys[key] = true;
-		else if (action == GLFW_RELEASE)
-			keys[key] = false;
-	}
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            keys[key] = false;
+    }
 }
 
 bool firstMouse = true;
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-	camera.ProcessMouseScroll(yoffset);
+    camera.ProcessMouseScroll(yoffset);
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -69,8 +69,8 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     GLfloat yoffset = ypos - lastY;
     lastX = xpos;
     lastY = ypos;
-
-	camera.ProcessMouseMovement(xoffset, yoffset);
+    
+    camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 
@@ -114,18 +114,24 @@ int main(int argc, char **argv)
     glfwSetKeyCallback(window, key_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     
     int width,height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     
     // Use shader class
-	std::string vs_path;
-	std::string ps_path;
-	common::GetShaderPath(vs_path, "advance.vs");
-	common::GetShaderPath(ps_path, "advance.ps");
+    std::string vs_path;
+    std::string ps_path;
+    common::GetShaderPath(vs_path, "advance.vs");
+    common::GetShaderPath(ps_path, "advance.ps");
     Shader outShader(vs_path.c_str(), ps_path.c_str());
+    
+    // screen shader: postprocess
+    std::string pVsPath, pPsPath;
+    common::GetShaderPath(pVsPath, "screen.vs");
+    common::GetShaderPath(pPsPath, "screen.ps");
+    Shader screenShader(pVsPath.c_str(), pPsPath.c_str());
     
     // create texures
     int tWidth, tHeight;
@@ -134,7 +140,7 @@ int main(int argc, char **argv)
     
     GLuint *textures = new GLuint[texture_length];
     glGenTextures(texture_length, textures);
-
+    
     for (int i = 0; i < texture_length; ++i) {
         glBindTexture(GL_TEXTURE_2D, textures[i]);
         // set texture wrapping mode
@@ -144,9 +150,9 @@ int main(int argc, char **argv)
         // set texture filtering mode
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		std::string path;
-		common::GetTexturePath(path, texture_file[i]);
+        
+        std::string path;
+        common::GetTexturePath(path, texture_file[i]);
         
         unsigned char *image = SOIL_load_image(path.c_str(), &tWidth, &tHeight, 0, SOIL_LOAD_RGB);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tWidth, tHeight, 0, GL_RGB,
@@ -211,21 +217,21 @@ int main(int argc, char **argv)
         -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
         5.0f,  -0.5f, -5.0f,  2.0f, 2.0f
     };
-
-
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3(2.0f, 5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f, 3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f, 2.0f, -2.5f),
-		glm::vec3(1.5f, 0.2f, -1.5f),
-		glm::vec3(-1.3f, 1.0f, -1.5f)
-	};
-
+    
+    
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)
+    };
+    
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -245,73 +251,124 @@ int main(int argc, char **argv)
     // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-
-	// create floor VAO
-	GLuint floorVAO, floorVBO;
-	glGenVertexArrays(1, &floorVAO);
-	glBindVertexArray(floorVAO);
+    
+    // create floor VAO
+    GLuint floorVAO, floorVBO;
+    glGenVertexArrays(1, &floorVAO);
+    glBindVertexArray(floorVAO);
     
     glGenBuffers(1, &floorVBO);
-	glBindBuffer(GL_ARRAY_BUFFER, floorVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, floorVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
     
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
-
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	std::cout << "111" << camera.Front.x << " " << camera.Front.y << " " << camera.Front.z << std::endl;
-
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_ALWAYS);
-   
+    glBindVertexArray(0);
+    
+    std::cout << "111" << camera.Front.x << " " << camera.Front.y << " " << camera.Front.z << std::endl;
+    
+    unsigned int renderTexture;
+    glGenTextures(1, &renderTexture);
+    glBindTexture(GL_TEXTURE_2D, renderTexture);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ScreenWidth, ScreenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, ScreenWidth, ScreenHeight);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderTexture, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout<< "frame buffer incomplete!" << std::endl;
+        return 0;
+    }
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    
+    float quadVertices[] = {
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 1.0f, 1.0f,
+    };
+    
+    unsigned int quardVAO, quardVBO;
+    glGenVertexArrays(1, &quardVAO);
+    glGenBuffers(1, &quardVBO);
+    glBindVertexArray(quardVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quardVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2*sizeof(float)));
+    glBindVertexArray(0);
+    
+    
+    
+    
     while (!glfwWindowShouldClose(window)) {
-		GLfloat current = glfwGetTime();
-		deltaTime = current - lastFrame;
-		lastFrame = current;
-
+        GLfloat current = glfwGetTime();
+        deltaTime = current - lastFrame;
+        lastFrame = current;
+        
         glfwPollEvents();
-		DoCameraMovements();
+        DoCameraMovements();
+        
+        /***********************************************************************************/
+        // first pass: draw to texture
+//        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+        glEnable(GL_DEPTH_TEST);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        // bind textures
         
-        // glActiveTexture(GL_TEXTURE0);
+        /* set cube shader and attrs */
+        outShader.Use();
+//        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, textures[1]);
         // We omit the glActiveTexture part since TEXTURE0 is already the default active texture unit. (sampler used in fragment is set to 0 as well as default)
-        //glUniform1i(glGetUniformLocation(outShader.Program,"texture0"), 0);
-        
-        outShader.Use();
-        
-		GLint modelLoc = glGetUniformLocation(outShader.Program, "model");
-		GLint viewLoc = glGetUniformLocation(outShader.Program, "view");
-		GLint projectLoc = glGetUniformLocation(outShader.Program, "projection");
+//        glUniform1i(glGetUniformLocation(outShader.Program,"texture0"), 0);
+        GLint modelLoc = glGetUniformLocation(outShader.Program, "model");
+        GLint viewLoc = glGetUniformLocation(outShader.Program, "view");
+        GLint projectLoc = glGetUniformLocation(outShader.Program, "projection");
         glm::mat4 view;
-		view = camera.GetViewMatrix();
-		glm::mat4 projection = camera.GetProjectionMatrix();
-
+        view = camera.GetViewMatrix();
+        glm::mat4 projection = camera.GetProjectionMatrix();
         
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projectLoc, 1, GL_FALSE, glm::value_ptr(projection));
-		
-
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        
+        
+        // draw cubes
         // cube1
-		glBindVertexArray(VAO);
-		glm::mat4 model;
+        glBindVertexArray(VAO);
+        glm::mat4 model;
         model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-        
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
         // cube2
         model = glm::mat4();
         model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
         
         // floor plane
@@ -322,17 +379,36 @@ int main(int argc, char **argv)
         glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
         
+        glfwSwapBuffers(window);
+        continue;
+        
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDisable(GL_DEPTH_TEST);
+        /***********************************************************************************/
+        
+        // pass 2: render the fbo bind texture to the quard
+        screenShader.Use();
+        glBindVertexArray(quardVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, renderTexture);
+        //glUniform1i(glGetUniformLocation(screenShader.Program, "texture0"), 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+        
         
         glfwSwapBuffers(window);
     }
     
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-
+    
     
     delete [] textures;
     
     glfwTerminate();
-	return 0;
+    return 0;
 }
+
 
