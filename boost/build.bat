fbab487758df3b2714871aad5158de886493ae48
xmandy <shenxm06@gmail.com>
2020-10-08 21:01:50 +0800

    Add Version Number.

--- boost/build.bat
+++ boost/build.bat
@@ -1,3 +1,4 @@
+rem use boost 1.73
 rem you must manually open vs command line tool to run it
 b2 install --prefix=".\BoostLib"  address-model=64 link=static runtime-link=static threading=multi architecture=x86 toolset=msvc-14.1 --build-type=complete
 pause
